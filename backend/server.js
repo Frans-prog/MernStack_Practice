f7a74fc3da07ca74d2eb4a9a3f5bd0141dd51b7d
Frans-prog <frlo.calimquim@gmail.com>
2024-09-26 16:18:07 +0800

    "*"

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ app.use('/api/cars', carRoutes )
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "/frontend/dist")))
-    app.get('/', (req, res) => {
+    
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     })
 }
